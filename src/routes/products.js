b462c8e1a2a31b6b3e99e6d2d08b4562336d06b5
Pedro <pedrohalmeidamendonca@gmail.com>
2023-03-22 18:56:22 -0300

    Requisito 04

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const productsController = require('../controllers/products');
+const productsValidation = require('../middlewares/products');
 
 const router = express.Router();
 
 router.get('/', productsController.getAll);
 router.get('/:id', productsController.getById);
-router.post('/', productsController.create);
+router.post('/', productsValidation.creationValidation, productsController.create);
 
 module.exports = router;
